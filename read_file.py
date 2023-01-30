bd76486d5e2cbe05558bc7338aaa3ad9e1fd8143
MixlimurodovShohruh <MixlimurodovShohruhElyorivech@gmail.com>
2023-01-30 19:57:08 +0500

    done

--- read_file.py
+++ read_file.py
@@ -9,10 +9,12 @@ def read_file(filename: str) -> list:
     Returns:
         data (list): A list of lines from the file.
     """
-    # Open the file
-    data = list(map(int,open(filename, "r").read().split(',')))
-    # Read the file
-    return data
-
-#Print list from file
-print(read_file("read_file_2.txt"))
+    s=g.split(',')
+    i=0
+    while i<len(s):
+        s[i] = int(s[i])
+        i+=1
+    return s
+f=open('data.txt', mode='r')
+g=f.read()
+print(read_file('data.txt'))
